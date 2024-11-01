1875c2d2e3619356d151c179a538420ee9f216fd
rhaniel99 <rhanielmonteiro.13@gmail.com>
2024-11-01 11:23:05 -0400

    adicionado o repositorio crud-student-api

---
+++ crud-student-api/src/routes/routes.js
@@ -0,0 +1,13 @@
+// const { Router } = require('express');
+// const controller = require('../config/controller');
+
+
+// const router = Router();
+
+// router.get("/", controller.getStudents);
+// router.post("/", controller.addStudent);
+// router.get("/:id", controller.getStudentsById);
+// router.put("/:id", controller.updateStudent);
+// router.delete("/:id", controller.removeStudent);
+
+// module.exports = router;
